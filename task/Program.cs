526726779f188b49ab89fa6cc7a96e8c3b529920
oksana <miss.okcana-chernova@yandex.ru>
2022-10-02 22:31:26 +0300

    3 commit

---
+++ task/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+int a = 3;
+string[] array2 = LengthElements(array);
+foreach (string i in array2)
+{
+    Console.Write(i + " ");
+}
+
+string[] LengthElements(string[] arr)
+{
+    int j = 0;
+    string[] array2 = new string[arr.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= a)
+        {
+            array2[j] = array[i];
+            j++;
+        }
+    }
+    return array2;
+}
